c280333d0df2cc3194eed0c33fa0c7c74a333e48
ThomasFaria <thomas.faria@ensae.fr>
2023-11-20 09:27:34 +0000

    squellette file

---
+++ src/preprocess-satellite-images.py
@@ -0,0 +1,8 @@
+# 1- download pour (dep/annee)
+# 2- Ouvrir avec SatelliteImage
+# 3- Labeliser avec labeler (labeler/tache)
+# 3.5- Creation masque nuages pour Pleiades et split masque
+# 4- Split les tuiles (param tiles_size)
+# 5- Filtre too black
+# 6- filtre nuages pour pleiades
+# 7- save dans data-prepro
