fc22c523e0d5bc5fd55d7f16eb9bcd991bd65c8a
ThomasFaria <thomas.faria@ensae.fr>
2023-11-23 16:38:34 +0000

    correct setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git config --global credential.helper store
 
-pre-commit install
-
 AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY_ID onyxia-kv/projet-slums-detection/s3` && export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_ACCESS_KEY onyxia-kv/projet-slums-detection/s3` && export AWS_SECRET_ACCESS_KEY
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
@@ -44,3 +42,4 @@ sudo apt-get update
 sudo apt-get install python3-gdal -y
 sudo apt-get install libgdal-dev -y
 pip install -r requirements.txt
+pre-commit install
