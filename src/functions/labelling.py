7d91c3b4c3b9969ca6ccc43a70337dd94a5e0dbb
ThomasFaria <thomas.faria@ensae.fr>
2023-12-05 16:42:35 +0000

    docstring

--- src/functions/labelling.py
+++ src/functions/labelling.py
@@ -2,7 +2,20 @@ from classes.labelers.labeler import BDTOPOLabeler
 
 
 def get_labeler(type_labeler, year, dep, task):
+    """
+    Get a labeler instance based on the specified type.
+
+    Parameters:
+    - type_labeler (str): The type of labeler to instantiate.
+    - year (int): The year parameter for labeler instantiation.
+    - dep (str): The dep parameter for labeler instantiation.
+    - task (str): The task parameter for labeler instantiation.
+
+    Returns:
+    - Labeler: An instance of the specified labeler type.
+    """
     labeler = None
+
     match type_labeler:
         case "BDTOPO":
             labeler = BDTOPOLabeler(year=year, dep=dep, task=task)
