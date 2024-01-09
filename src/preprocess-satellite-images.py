correct normalization

src/preprocess-satellite-images.py
@@ -28,7 +28,14 @@ def main(
     )
 
     print("\n*** 2- Annotation, découpage et filtrage des images...\n")
-    for im in tqdm(fs.ls(f"projet-slums-detection/data-raw/{source}/{dep}/{year}/")):
+
+    # Instanciate a dict of metrics for normalization
+    metrics = {
+        "mean": [],
+        "std": [],
+    }
+
+    for im in tqdm(fs.ls(f"projet-slums-detection/data-raw/{source}/{dep}/{year}/")[300:310]):
         # 1- Ouvrir avec SatelliteImage
         si = SatelliteImage.from_raster(
             file_path=f"/vsis3/{im}",
@@ -67,12 +74,6 @@ def main(
             )
         ]
 
-        # Instanciate a dict of metrics for normalization
-        metrics = {
-            "mean": [],
-            "std": [],
-        }
-
         # 5- save dans data-prepro
         for i, lsi in enumerate(splitted_lsi_filtered):
             filename, ext = os.path.splitext(os.path.basename(im))
@@ -87,16 +88,16 @@ def main(
             metrics["mean"].append(np.mean(lsi.satellite_image.array, axis=(1, 2)))
             metrics["std"].append(np.std(lsi.satellite_image.array, axis=(1, 2)))
 
-        metrics["mean"] = np.vstack(metrics["mean"]).mean(axis=0).tolist()
-        metrics["std"] = np.vstack(metrics["std"]).mean(axis=0).tolist()
-        yaml_data = yaml.dump(metrics, default_flow_style=False)
+    metrics["mean"] = np.vstack(metrics["mean"]).mean(axis=0).tolist()
+    metrics["std"] = np.vstack(metrics["std"]).mean(axis=0).tolist()
+    yaml_data = yaml.dump(metrics, default_flow_style=False)
 
-        # Save metrics file in s3
-        with fs.open(
-            f"projet-slums-detection/data-preprocessed/patchs/{task}/{source}/{dep}/{year}/{tiles_size}/metrics-normalization.yaml",
-            "wb",
-        ) as f:
-            f.write(yaml_data.encode("utf-8"))
+    # Save metrics file in s3
+    with fs.open(
+        f"projet-slums-detection/data-preprocessed/patchs/{task}/{source}/{dep}/{year}/{tiles_size}/metrics-normalization.yaml",
+        "wb",
+    ) as f:
+        f.write(yaml_data.encode("utf-8"))
 
     print("\n*** 3- Preprocessing terminé !\n")
 